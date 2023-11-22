212ca65ba4ff55d52935b8193fe9ed70a407a499
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@MacBook-Pro-de-Marie-Tiffany.local>
2023-11-22 15:20:36 +0100

    fichier makefile ajouté

---
+++ commandes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdint.h>
+#include<dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+struct commandes
+{
+    char commande[30];
+};
+/*
+
+char *pwd(){}
+
+void ls(char PATH){}
+
+char *cd(char PATH){}
+
+void cp(char fic1,char fic2){}
+
+void mv(char fic1,char fic2){}
+*/
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<stdint.h>
+#include<dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
