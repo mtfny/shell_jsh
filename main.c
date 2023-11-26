35c76af08b6a94f2929ff806b307b47938d4217a
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@MacBook-Pro-de-Marie-Tiffany.local>
2023-11-26 18:39:23 +0100

    debut boucle principale et prompt sans les couleurs

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include<stdint.h>
+#include <string.h>
 #include<dirent.h>
 #include <errno.h>
 #include <sys/stat.h>
@@ -8,10 +9,64 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 
+#define MAX_PROMPT_LENGTH 30
+#define MAX_PATH_LENGTH 512
+
+void prompt(const char *path){
+    char prompt[MAX_PROMPT_LENGTH];
+
+    // On vérifie la longueur
+    if (strlen(path) > (MAX_PROMPT_LENGTH - 4)) {
+             snprintf(prompt, sizeof(prompt), " ... %s  ", path + strlen(path) - (MAX_PROMPT_LENGTH - 7)); 
+    }else{
+         snprintf(prompt, sizeof(prompt), "%s ", path);
+    } 
+
+    // Ajoute le prompt à l'historique
+    add_history(prompt);
+
+    // Affiche le prompt
+    fprintf(stderr, "%s", prompt);
+}
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+    rl_outstream = stderr;
+    char *line ;
+
+    char *chemin_courant = getcwd(NULL,0);
+
+    while (1)
+    {
+        prompt(chemin_courant);
+        line = readline("$");
+        
+        if (!line) {
+            // L'utilisateur a appuyé sur Ctrl-D pour quitter
+            printf("\n");
+            break;
+        }
+
+        // Ajout de la ligne à l'historique
+        add_history(line);
+
+        // 
+
+        // Libération de la mémoire allouée par readline
+        free(line);
+       
+    }
+
+
+    
+
+    
     return 0;
 }
+
+
+
+void job_update(){}
